450174a501aa5261aae95eb43eacf1ee9cc3794a
donalod <irldexter@podomere.com>
2021-02-11 17:39:28 +0000

    Updating to p3 folder

--- pansift_scripts_update.sh
+++ pansift_scripts_update.sh
@@ -2,4 +2,4 @@
 
 # Note: GitHub serves "raw" pages with Cache-Control: max-age=300. That's specified in seconds, meaning the pages are intended to be cached for 5 minutes. You can see this if you open the Developer Tools in your web browser of choice before clicking the "Raw" button on GitHub.
 
-curl -s https://raw.githubusercontent.com/pansift/p3-agent/main/osx_network_default_script.sh > $HOME/.pansift/osx_network_default_script.sh
+curl -s https://raw.githubusercontent.com/pansift/p3/main/osx_network_default_script.sh > $HOME/.pansift/osx_network_default_script.sh
