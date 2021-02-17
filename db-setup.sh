c99c4de8205eb9eabfa4aaaf03d30ed9d92691ce
donalod <irldexter@podomere.com>
2021-02-17 14:11:29 +0000

    Playing with the web router concept

--- db-setup.sh
+++ db-setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-url="http://aeuw1-p3t0002.infra.p3.pansift.com:9000/hooks/setup"
+url="http://webrouter.infra.p3.pansift.com:443/hooks/setup"
 token=$(curl -s --data "uuid=$1" $url | cut -d',' -f3 | tr -d '\r')
 [[ "${#token}" -eq 88 ]] && export pansift_token=$token && echo "$token" || echo "null"
