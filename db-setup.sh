09af63996b9e05fc034a3330b6454a74f01c299e
donalod <irldexter@podomere.com>
2021-02-17 14:42:20 +0000

    Re-doing some of the ingestion and webhooks

--- db-setup.sh
+++ db-setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-url="http://webrouter.infra.p3.pansift.com:443/hooks/setup"
-token=$(curl -s --data "uuid=$1" $url | cut -d',' -f3 | tr -d '\r')
-[[ "${#token}" -eq 88 ]] && export pansift_token=$token && echo "$token" || echo "null"
+# Require an UUID argument or just exit.
+
+if [[ $1 =~ ^\{?[A-F0-9a-f]{8}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{12}\}?$ ]]; then
+  url="http://webrouter.infra.p3.pansift.com:443/hooks/setup"
+  token=$(curl -s --data "uuid=$1" $url | cut -d',' -f3 | tr -d '\r')
+  [[ "${#token}" -eq 88 ]] && export pansift_token=$token && echo "$token" || echo "null"
+else
+  exit 0
+fi
--- ingest.sh
+++ ingest.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-url="http://webrouter.infra.p3.pansift.com:443/hooks/ingestrouter"
-ingest=$(curl -s --data "uuid=$1" $url | tr -d '\r')
-regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
-if [[ $ingest =~ $regex ]]
-then 
+# Require an UUID argument or just return localhost
+
+# This requires connectivity, what about when no connectivity and there's a restart?
+
+if [[ $1 =~ ^\{?[A-F0-9a-f]{8}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{12}\}?$ ]]; then
+  url="http://webrouter.infra.p3.pansift.com:443/hooks/ingestrouter"
+  ingest=$(curl -s --data "uuid=$1" $url | tr -d '\r')
+  regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
+  if [[ $ingest =~ $regex ]]
+  then 
     echo $ingest
+  else
+    exit
+  fi
 else
-    echo $pansift_default_ingest
+  echo "http://localhost"
 fi
