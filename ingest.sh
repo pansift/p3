efb3defd908ee6671ffead5bea66f92e9ff691ee
donalod <irldexter@podomere.com>
2021-02-17 15:26:42 +0000

    Updates now that running a load balancer

--- ingest.sh
+++ ingest.sh
@@ -5,7 +5,7 @@
 # This requires connectivity, what about when no connectivity and there's a restart?
 
 if [[ $1 =~ ^\{?[A-F0-9a-f]{8}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{12}\}?$ ]]; then
-  url="http://webrouter.infra.p3.pansift.com:443/hooks/ingestrouter"
+  url="http://webrouter.pansift.com:443/hooks/ingestrouter"
   ingest=$(curl -s --data "uuid=$1" $url | tr -d '\r')
   regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
   if [[ $ingest =~ $regex ]]
