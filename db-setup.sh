c65ee74d197d830e9b982d0ae8393adbb83560b5
donalod <irldexter@podomere.com>
2021-02-20 17:28:37 +0000

    New db-setup endpoint

--- db-setup.sh
+++ db-setup.sh
@@ -3,7 +3,7 @@
 # Require an UUID argument or just exit.
 
 if [[ $1 =~ ^\{?[A-F0-9a-f]{8}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{12}\}?$ ]]; then
-  url="http://webrouter.infra.p3.pansift.com:443/hooks/setup"
+  url="https://webrouter.pansift.com/hooks/setup"
   token=$(curl -s --data "uuid=$1" $url | cut -d',' -f3 | tr -d '\r')
   [[ "${#token}" -eq 88 ]] && export pansift_token=$token && echo "$token" || echo "null"
 else
