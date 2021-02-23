2b52acde0c72b78a1c870083cf73fa06bba4d603
donalod <irldexter@podomere.com>
2021-02-23 14:00:45 +0000

    Passing UUID to webapp open and scripts pbar fix

--- pansift_webapp.sh
+++ pansift_webapp.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-open https://app.pansift.com
+pansift_uuid_file=$HOME/p3/pansift_uuid.conf
+if test -f "$pansift_uuid_file"; then
+  line=$(head -n 1 "$pansift_uuid_file")
+  pansift_uuid=$(echo -n "$line" | awk '{$1=$1;print}' | tr ',' '.' | tr -s ' ' | tr '[:upper:]' '[:lower:]' | tr -d '\r')
+fi
+
+open https://app.pansift.com?uuid=${pansift_uuid}
