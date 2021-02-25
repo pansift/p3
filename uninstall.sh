5b2b51f969f0d3c63b220b7d6915622fbc60f23f
donalod <irldexter@podomere.com>
2021-02-25 14:42:25 +0000

    Tweaking uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -9,30 +9,45 @@
 defaults delete com.matryer.BitBar
 
 # Configuration and preferences files
-PANSIFT_PREFERENCES="$HOME/Library/Preferences/Pansift"
+PANSIFT_PREFERENCES="$HOME"/Library/Preferences/Pansift
 
 # Scripts and additional executables
-PANSIFT_SCRIPTS="$HOME/Library/Application Scripts/Pansift"
+PANSIFT_SCRIPTS="$HOME"/Library/Application\ Scripts/Pansift
 
 # Logs, logs, logs
-PANSIFT_LOGS="$HOME/Library/Logs/Pansift"
+PANSIFT_LOGS="$HOME"/Library/Logs/Pansift
 
 # PIDs and other flotsam
-PANSIFT_SUPPORT="$HOME/Library/Application Support/Pansift"
+PANSIFT_SUPPORT="$HOME"/Library/Application\ Support/Pansift
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   echo "Not supported on Linux yet" 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   # Mac OSX
-  # scripts to Trash
-  mv "$PANSIFT_SCRIPTS" "$HOME"/.Trash
-  # conf files
+  # Scripts to Trash
+  cp -R "$PANSIFT_SCRIPTS" "$HOME"/.Trash
+  cd "$PANSIFT_SCRIPTS" && rm -rf ../Pansift
+  cd .. && rmdir ./Pansift
+  cd "$HOME"
+  # Conf files
   rm "$PANSIFT_PREFERENCES"/pansift_token.conf 
-  mv "$PANSIFT_PREFERENCES" "$HOME"/.Trash
-  # app to /Applications
-  mv /Applications/Pansift.app "$HOME"/.Trash
+  cp -R "$PANSIFT_PREFERENCES" "$HOME"/.Trash
+  cd "$PANSIFT_PREFRENCES" && rm -rf ../Pansift
+  cd .. && rmdir ./Pansift
+  cd "$HOME"
+  # /Applications
+  cp -R /Applications/Pansift.app "$HOME"/.Trash
+  cd /Applications && rm -rf Pansift.app
+  # Logs
+  cp -R "$PANSIFT_LOGS" "$HOME"/.Trash
+  cd "$PANSIFT_LOGS" && rm -rf ../Pansift
+  cd .. && rmdir ./Pansift
+  cd "$HOME"
   # Telegraf Support
-  mv "$PANSIFT_SUPPORT" "$HOME"/.Trash
+  cp -R "$PANSIFT_SUPPORT" "$HOME"/.Trash
+  cd "$PANSIFT_SUPPORT" && rm -rf ../Pansift 
+  cd .. && rmdir ./Pansift
+  cd "$HOME"
 elif [[ "$OSTYPE" == "cygwin" ]]; then
   # POSIX compatibility layer and Linux environment emulation for Windows
   echo "Not supported on Cygwin yet" 
