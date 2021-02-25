bb9d7380c1c7e74069405866272ed9bb065d5b01
donalod <irldexter@podomere.com>
2021-02-25 12:11:35 +0000

    Spurious export

--- db-setup.sh
+++ db-setup.sh
@@ -5,7 +5,7 @@
 if [[ $1 =~ ^\{?[A-F0-9a-f]{8}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{12}\}?$ ]]; then
   url="https://webrouter.pansift.com/hooks/setup"
   token=$(curl -k -s --data "uuid=$1" $url | cut -d',' -f3 | tr -d '\r')
-  [[ "${#token}" -eq 88 ]] && export pansift_token=$token && echo "$token" || echo "null"
+  [[ "${#token}" -eq 88 ]] && echo "$token" || echo "null"
 else
   exit 0
 fi
