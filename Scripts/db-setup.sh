c42906c0220e3de24bb6f4327c8ec71620fe62f0
donalod <irldexter@podomere.com>
2021-03-05 13:29:30 +0000

    Moving to a new ztp remote script

--- Scripts/db-setup.sh
+++ Scripts/db-setup.sh
@@ -3,14 +3,24 @@
 # Require an UUID argument or just exit.
 
 if [[ $1 =~ ^\{?[A-F0-9a-f]{8}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{12}\}?$ ]]; then
-  url="https://webrouter.pansift.com/hooks/setup"
+  url="https://webrouter.pansift.com/hooks/ztp"
   uuid=$(echo -n "$1" | tr '[:upper:]' '[:lower:]')
-  token=$(curl -k -s --data "uuid=$uuid" $url | cut -d',' -f2 | tr -d '\r')
+  user_agent="pansift-"$uuid
+  curl_response=$(curl -A "$user_agent" -k -s --data "uuid=$uuid" $url)
+  token=$(echo -n "$curl_response" | cut -d',' -f2 | tr -d '\r')
+  ingest=$(echo -n "$curl_response" | cut -d',' -f3 | tr -d '\r')
   if [[ $token =~ ^[-_A-Z0-9a-z]{86}==$ ]]; then 
-    echo -n "$token"
+    ingest_regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
+    if [[ $ingest =~ $ingest_regex ]]; then
+      echo -n "${token},${ingest}"
+    else
+      echo "null,null,ingest_url_problem"
+      exit 0
+    fi    
   else
-    echo "null"
+    echo "null,null,token_format_problem"
   fi
 else
+  echo "null,null,arg_format_problem"
   exit 0
 fi
