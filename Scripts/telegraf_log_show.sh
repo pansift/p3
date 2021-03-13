b2ef01577531adacc2f92bc0b38e4e88f38cf882
donalod <donal@pansift.com>
2021-03-13 14:09:00 +0000

    Trying to open terminal in Big Sur

---
+++ Scripts/telegraf_log_show.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#set -e
+#set -vx
+applescriptCode=""
+source "$HOME"/Library/Preferences/Pansift/pansift.conf
+
+applescriptCode='tell application "Terminal" to do script "tail -f -n50 $HOME/Library/Logs/Pansift/telegraf.log" & (activate) & return'
+
+show=$(osascript -e "$applescriptCode");
+
