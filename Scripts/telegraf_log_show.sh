349e0634c834d7865ab46ddb98217dc4fab5fede
donalod <donal@pansift.com>
2021-03-13 14:28:05 +0000

    Better /Users/laptop encapsulation

--- Scripts/telegraf_log_show.sh
+++ Scripts/telegraf_log_show.sh
@@ -5,7 +5,7 @@
 applescriptCode=""
 source "$HOME"/Library/Preferences/Pansift/pansift.conf
 
-applescriptCode='tell application "Terminal" to do script "tail -f -n50 $HOME/Library/Logs/Pansift/telegraf.log" & (activate) & return'
+applescriptCode='tell application "Terminal" to do script "tail -f -n50 \"$HOME\"/Library/Logs/Pansift/telegraf.log" & activate & return'
 
 show=$(osascript -e "$applescriptCode");
 
