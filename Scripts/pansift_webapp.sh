f6aa4a1fc4fc2af8d57dd435d43e2792dee78437
donalod <donal@pansift.com>
2021-03-20 16:40:02 +0000

    Web open script tweak

--- Scripts/pansift_webapp.sh
+++ Scripts/pansift_webapp.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-source "$HOME"/Library/Preferences/Pansift/pansift.conf
-
-pansift_uuid_file=$PANSIFT_PREFERENCES/pansift_uuid.conf
-if test -f "$pansift_uuid_file"; then
-  line=$(head -n 1 "$pansift_uuid_file")
-  pansift_uuid=$(echo -n "$line" | awk '{$1=$1;print}' | tr ',' '.' | tr -s ' ' | tr '[:upper:]' '[:lower:]' | tr -d '\r')
+if [[ "$1" == "-w" ]]; then
+	open https://app.pansift.com
+	exit
+else
+	source "$HOME"/Library/Preferences/Pansift/pansift.conf
+	pansift_uuid_file=$PANSIFT_PREFERENCES/pansift_uuid.conf
+	if test -f "$pansift_uuid_file"; then
+		line=$(head -n 1 "$pansift_uuid_file")
+		pansift_uuid=$(echo -n "$line" | awk '{$1=$1;print}' | tr ',' '.' | tr -s ' ' | tr '[:upper:]' '[:lower:]' | tr -d '\r')
+	fi
+	open https://app.pansift.com?uuid=${pansift_uuid}
+	exit 
 fi
 
-open https://app.pansift.com?uuid=${pansift_uuid}
