a7c197163943156c17648cb0a2872271dfa1aa24
donalod <donal@pansift.com>
2023-11-08 18:01:53 +0000

    Fix for log open in later macOS

--- Scripts/telegraf_log_show.sh
+++ Scripts/telegraf_log_show.sh
@@ -2,6 +2,6 @@
 
 #set -e
 #set -vx
-source "$HOME"/Library/Preferences/Pansift/pansift.conf
+# source "$HOME"/Library/Preferences/Pansift/pansift.conf
 
-osascript -e 'tell application "Terminal"' -e 'if (exists window 1) and not busy of window 1 then' -e 'do script "tail -f -n50 \"$HOME\"/Library/Logs/Pansift/telegraf.log"' -e 'else' -e 'do script "tail -f -n50 \"$HOME\"/Library/Logs/Pansift/telegraf.log"' -e 'end if' -e 'activate' -e 'end tell'
+osascript -e 'tell application "Terminal"' -e 'if (exists window 1) and not (busy of window 1) then' -e 'do script "tail -f -n100 \"$HOME\"/Library/Logs/Pansift/telegraf.log"' -e 'activate' -e 'else' -e 'do script "tail -f -n100 \"$HOME\"/Library/Logs/Pansift/telegraf.log"' -e 'activate' -e 'end if' -e 'end tell'
