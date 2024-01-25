e707e0cfc120037f451c32c57019058a730eddd4
donalod <donal@pansift.com>
2024-01-25 17:20:52 +0000

    A grossly simplified log open

--- Scripts/telegraf_log_show.sh
+++ Scripts/telegraf_log_show.sh
@@ -4,19 +4,15 @@
 #set -vx
 # source "$HOME"/Library/Preferences/Pansift/pansift.conf
 
-osascript  \
--e 'tell application "Terminal"' \
-		-e 'activate' \
-		-e 'delay 1' \
-		-e 'if (exists window 1) and (busy of window 1) then' \
-  	-e 'activate' \
-  	-e 'do script "tail -f -n100 \"$HOME\"/Library/Logs/Pansift/telegraf.log"' \
-	-e 'else if (exists window 1) and not (busy of window 1) then' \
-		-e 'activate' \
-		-e 'do script "tail -f -n100 \"$HOME\"/Library/Logs/Pansift/telegraf.log" in window 1' \
-	-e 'else if not (exists window 1) then' \
-		-e 'do script "tail -f -n100 \"$HOME\"/Library/Logs/Pansift/telegraf.log"' \
-	-e 'else' \
-		-e 'display alert "PanSift Error" message "PanSift could not open a Terminal window"' \
-	-e 'end if' \
--e 'end tell'
+# TODO: This could write a tail command in to someone's active terminal window session.
+# Previous attempts at more comprehensive logic result in a problem with "busy" on Sonoma
+# Currently, trying to simplify without overthinking?
+
+open "$HOME"/Library/Logs/Pansift/telegraf.log
+
+# osascript  \
+# -e 'tell application "Terminal"' \
+# 		-e 'if not (exists window 1) then reopen' \
+# 		-e 'activate' \
+# 		-e 'do script "tail -f -n100 \"$HOME\"/Library/Logs/Pansift/telegraf.log" in window 1' \
+# -e 'end tell'
